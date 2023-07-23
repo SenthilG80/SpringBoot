e7fba5482649335671d1698adc5b096f8d04e42e
Pratosh Krishnan <Pratosh Krishnan@DESKTOP-JE470RL>
2023-07-23 21:16:51 +0530

    push

---
+++ MVCProj4--SpringBootMVC-Datarendering/src/main/java/com/nt/controller/SeasonsOperationsController.java
@@ -0,0 +1,28 @@
+package com.nt.controller;
+
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import com.nt.service.ISeasonFinderService;
+
+
+
+@Controller
+public class SeasonsOperationsController {
+	
+	@Autowired
+	private ISeasonFinderService service;
+	
+	@RequestMapping("/")
+	public String showHome() {
+		return "welcome";
+	}
+	
+	@RequestMapping("/season")
+	public String showSeason(Map<String,Object> map) {
+		String msg=service.findSeason();
+		map.put("resultMsg",msg);
+		return "display";
+	}
+}
---
+++ MVCProj4--SpringBootMVC-Datarendering/src/main/webapp/WEB-INF/pages/welcome.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body  style="background-color: black;">
+ <h1 style="color:yellow;text-align:center">WELCOME.JSP(HOME PAGE)</h1>
+ <h1 style="color:white;text-align:center"><a href="season">
+   <button style="background-color: red; color: white; padding: 10px 20px; border: none; cursor: pointer; font-size: 16px;">
+ FIND SEASON
+ </button>
+ </a></h1>
+</body>
+</html>
