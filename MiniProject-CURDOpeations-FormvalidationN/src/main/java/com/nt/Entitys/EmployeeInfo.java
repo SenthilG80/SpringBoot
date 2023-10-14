f40b3542b3180cc1853f05395c6883d3f4a402f9
Pratosh Krishnan <Pratosh Krishnan@DESKTOP-JE470RL>
2023-10-15 00:32:45 +0530

    Push

---
+++ MiniProject-CURDOpeations-FormvalidationN/src/main/java/com/nt/Entitys/EmployeeInfo.java
@@ -0,0 +1,35 @@
+package com.nt.Entitys;
+
+
+
+
+
+
+import java.io.Serializable;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name="EMP")
+@Data
+public class EmployeeInfo implements Serializable{
+	@Id
+	@SequenceGenerator(name="gen1",sequenceName ="emp_id_seq",allocationSize = 1,initialValue = 251 )
+	@GeneratedValue(generator = "gen1",strategy = GenerationType.SEQUENCE)
+	private Integer empNo;
+	
+	@Column(length = 20)
+	private String ename;
+	private Double sal;
+	@Column(length = 20)
+	private String job="CLERK";
+    private Integer deptno=10;
+	
+	
+}
---
+++ MiniProject-CURDOpeations-FormvalidationN/src/main/java/com/nt/validator/EmployeeInfoValidator.java
@@ -0,0 +1,45 @@
+package com.nt.validator;
+
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+import com.nt.Entitys.EmployeeInfo;
+
+@Component
+public class EmployeeInfoValidator implements Validator {
+
+	@Override
+	public boolean supports(Class<?> clazz) {
+		// TODO Auto-generated method stub
+		return clazz.isAssignableFrom(EmployeeInfo.class);
+	}
+
+	@Override
+	public void validate(Object target, Errors errors) {
+		// TODO Auto-generated method stub
+       EmployeeInfo emp=(EmployeeInfo)target;
+       
+       if(emp.getEname()==null || emp.getEname().length()==0 ||emp.getEname()=="")
+    	   errors.rejectValue("ename", "emp.name.required");
+       else if (emp.getEname().length()<5 || emp.getEname().length() >15)
+    	   errors.rejectValue("ename", "emp.name.length");
+       
+       if(emp.getJob()==null || emp.getJob().length()==0 ||emp.getJob()=="")
+    	   errors.rejectValue("job", "emp.job.required");
+       else if (emp.getJob().length()<5 || emp.getJob().length() >10)
+    	   errors.rejectValue("job", "emp.job.length");
+       
+       
+       if(emp.getSal()==null )
+    	   errors.rejectValue("sal", "emp.salary.required");
+       else if (emp.getSal()<10000 || emp.getSal() >200000)
+    	   errors.rejectValue("sal", "emp.salary.range");
+       
+       if(emp.getDeptno()==null )
+    	   errors.rejectValue("deptno", "emp.deptno.required");
+
+       
+	}
+
+}
